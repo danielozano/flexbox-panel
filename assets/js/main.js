743ddef74a6f83b4f8d778e1ae0e6c762ac1eeca
Daniel Lozano <dn.lozano.m@gmail.com>
2016-04-02 14:06:56 +0200

    añadir/eliminar elemento flex. Interruptor menu de los items.
    Base HTML5 básica creada. Añadido menú adicional a los elementos flex, y
    capacidad de añadir/eliminar elementos hijos al contenedor flex. Aún
    queda añadir las propiedades y la estructura Html a dichos hijos.

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+/**
+ * TODO: cambiar ancho en función del input:range
+ * TODO: crear y bindear las propiedaded flex de cada elemento hijo
+ * TODO: permitir añadir/eliminar objetos flex y reiniciar
+ * TODO: permitir cambiar las opciones flex del padre.
+ */
+window.onload = (function() {
+	'use strict';
+
+	var addButton = document.getElementById('add-box');
+	var deleteButton = document.getElementById('delete-box');
+	var content = document.getElementById('box-content');
+	var flexContainer = document.getElementById('flexbox-container');
+	var flexItems = document.getElementsByClassName('flex-item');
+	var optionsToggler = document.getElementsByClassName('flex-item-toggle');
+
+	// inicio contenedor flexbox
+	/**
+	 * Bindear los botones de añadir/eliminar elemento
+	 * NOTE: quizás el borrar un elemento debería ir dentro del propio flex-item
+	 *
+	 * Para añadir un elemento hay que tomar el contenido HTML del textarea y
+	 * crear un HTML idéntico al de los flex-items e introducir dicho contenido
+	 * como cuerpo del elemento.
+	 *
+	 * Una vez añadido quizás haya que volver a 're-bindear' las opciones
+	 */
+	flexContainer.addItem = function (event = null, parent = null) {
+		var parent = parent;
+		var node = document.createElement('div');
+		// TODO: crear html interior
+		node.innerHTML = '<p>Hello World</p>';
+
+		if (null === parent) {
+			parent = this;
+		}
+		this.appendChild(node);
+		event.preventDefault();
+	};
+
+	flexContainer.removeItem = function (event = null, child = null) {
+		var lastChild = child;
+		if (null === child) {
+			lastChild = this.lastElementChild;
+		}
+		this.removeChild(lastChild);
+		event.preventDefault();
+	};
+	// Bindear eventos para añadir/borrar nuevos elementos flex al contenedor
+	addButton.addEventListener('click', function(e) { flexContainer.addItem(e, null) });
+	deleteButton.addEventListener('click', function(e) { flexContainer.removeItem(e, null); });
+	// ----------- fin contendor flexbox
+	
+	// inicio elementos flexbox
+	/**
+	 * Bindear todos los interruptores de opciones individuales 
+	 * de cada flexbox item. Así se podrán modificar individualmente
+	 * las opciones de cada item
+	 */
+	for (var i = 0; i < optionsToggler.length; i++) {
+		optionsToggler[i].addEventListener('click', function(event) {
+			// Obtener el hermano más cercano, que en este caso es el menú de opciones.
+			var sibling = this.nextElementSibling;
+			sibling.classList.toggle('hide');
+		}, false);
+	}
+
+})();
